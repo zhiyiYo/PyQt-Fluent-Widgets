5aa049eeda485c4ab0a3534c8b763966479ec062
zhiyiYo <1319158137@qq.com>
2023-03-03 21:48:24 +0800

    调整无边框对话框样式，添加创建提示条类方法

--- qfluentwidgets/__init__.py
+++ qfluentwidgets/__init__.py
@@ -10,7 +10,7 @@ online at https://pyqt-fluent-widgets.readthedocs.io.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 from .components import *
 from .common import *
