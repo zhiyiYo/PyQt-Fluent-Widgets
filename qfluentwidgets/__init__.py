7d2c563d37897d7ece062611156c323de8a123be
zhiyiYo <1319158137@qq.com>
2023-03-25 14:48:39 +0800

    更新版本号

--- qfluentwidgets/__init__.py
+++ qfluentwidgets/__init__.py
@@ -12,7 +12,7 @@ Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/Py
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .components import *
 from .common import *
