f62e3f99864a6df2fa89fa399dfb23aafdbff80d
zhiyiYo <1319158137@qq.com>
2023-03-27 09:32:32 +0800

    修复设置卡中滑动条宽度过小的问题

--- qfluentwidgets/components/settings/setting_card.py
+++ qfluentwidgets/components/settings/setting_card.py
@@ -169,7 +169,6 @@ class RangeSettingCard(SettingCard):
         self.configItem = configItem
         self.slider = Slider(Qt.Horizontal, self)
         self.valueLabel = QLabel(self)
-        self.slider.setFixedWidth(268)
         self.slider.setMinimumWidth(268)
 
         self.slider.setSingleStep(1)
