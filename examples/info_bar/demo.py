fa376693ccce2002ae576aa4b99b0fffbcc8e48a
zhiyiYo <1319158137@qq.com>
2023-04-01 13:23:32 +0800

    修复水平布局的消息条间距过大的问题

--- examples/info_bar/demo.py
+++ examples/info_bar/demo.py
@@ -17,22 +17,25 @@ class Demo(QWidget):
         self.button2 = PushButton('Success', self)
         self.button3 = PushButton('Warning', self)
         self.button4 = PushButton('Error', self)
+        self.button5 = PushButton('Custom', self)
 
         self.button1.clicked.connect(self.createInfoInfoBar)
         self.button2.clicked.connect(self.createSuccessInfoBar)
         self.button3.clicked.connect(self.createWarningInfoBar)
         self.button4.clicked.connect(self.createErrorInfoBar)
+        self.button5.clicked.connect(self.createCustomInfoBar)
 
         self.hBoxLayout.addWidget(self.button1)
         self.hBoxLayout.addWidget(self.button2)
         self.hBoxLayout.addWidget(self.button3)
         self.hBoxLayout.addWidget(self.button4)
+        self.hBoxLayout.addWidget(self.button5)
         self.hBoxLayout.setContentsMargins(30, 0, 30, 0)
 
         self.resize(700, 700)
 
     def createInfoInfoBar(self):
-        content = "A long essential app message for your users to be informed of, acknowledge, or take action on. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus dolor vitae justo rutrum, ut lobortis nibh mattis. Aenean id elit commodo, semper felis nec."
+        content = "My name is kira yoshikake, 33 years old. Living in the villa area northeast of duwangting, unmarried. I work in Guiyou chain store. Every day I have to work overtime until 8 p.m. to go home. I don't smoke. The wine is only for a taste. Sleep at 11 p.m. for 8 hours a day. Before I go to bed, I must drink a cup of warm milk, then do 20 minutes of soft exercise, get on the bed, and immediately fall asleep. Never leave fatigue and stress until the next day. Doctors say I'm normal."
         w = InfoBar(
             icon=InfoBarIcon.INFORMATION,
             title='Title',
@@ -47,11 +50,10 @@ class Demo(QWidget):
         w.show()
 
     def createSuccessInfoBar(self):
-        content = "A short essential success app message."
-        # convenient static mothod
+        # convenient class mothod
         InfoBar.success(
-            title='Title',
-            content=content,
+            title='Lesson 4',
+            content="With respect, let's advance towards a new stage of the spin.",
             orient=Qt.Horizontal,
             isClosable=True,
             position=InfoBarPosition.TOP,
@@ -61,8 +63,8 @@ class Demo(QWidget):
 
     def createWarningInfoBar(self):
         InfoBar.warning(
-            title='Title',
-            content="A short essential app warning message.",
+            title='Lesson 3',
+            content="Believe in the spin, just keep believing!",
             orient=Qt.Horizontal,
             isClosable=False,   # disable close button
             position=InfoBarPosition.TOP_LEFT,
@@ -72,14 +74,27 @@ class Demo(QWidget):
 
     def createErrorInfoBar(self):
         InfoBar.error(
-            title='Title',
-            content="A short essential app error message.",
+            title='Lesson 5',
+            content="迂回路を行けば最短ルート。",
             orient=Qt.Horizontal,
             isClosable=True,
             position=InfoBarPosition.BOTTOM_RIGHT,
             duration=-1,    # won't disappear automatically
             parent=self
         )
+
+    def createCustomInfoBar(self):
+        w = InfoBar.new(
+            icon=FluentIcon.GITHUB,
+            title='Zeppeli',
+            content="人間讃歌は「勇気」の讃歌ッ！！ 人間のすばらしさは勇気のすばらしさ！！",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM,
+            duration=2000,
+            parent=self
+        )
+        w.setCustomBackgroundColor('white', '#202020')
 
 
 if __name__ == '__main__':
