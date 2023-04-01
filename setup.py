46d2b9b9e59145f5e07db7479e1e0c0b18611e31
zhiyiYo <1319158137@qq.com>
2023-04-01 13:20:51 +0800

    修复水平布局的消息条间距过大的问题

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name="PyQt6-Fluent-Widgets",
-    version="0.2.8",
+    version="0.2.9",
     keywords="pyqt6 fluent widgets",
     author="zhiyiYo",
     author_email="shokokawaii@outlook.com",
