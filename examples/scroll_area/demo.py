cb912afe3cd4e52b1d54eef73d381e9c34352cc4
zhiyiYo <1319158137@qq.com>
2023-04-02 19:44:31 +0800

    fixes #67

--- examples/scroll_area/demo.py
+++ examples/scroll_area/demo.py
@@ -2,15 +2,15 @@
 import sys
 from PyQt6.QtCore import QEasingCurve, Qt
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QApplication, QLabel
-from qfluentwidgets import SmoothScrollArea
+from PyQt6.QtWidgets import QApplication
+from qfluentwidgets import SmoothScrollArea, PixmapLabel
 
 
 class Demo(SmoothScrollArea):
 
     def __init__(self):
         super().__init__()
-        self.label = QLabel(self)
+        self.label = PixmapLabel(self)
         self.label.setPixmap(QPixmap("resource/shoko.jpg"))
 
         # customize scroll animation
