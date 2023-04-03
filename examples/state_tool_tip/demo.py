c564967ad83d40647f7114343689176f462c2d83
zhiyiYo <1319158137@qq.com>
2023-04-03 09:19:07 +0800

    调整进度提示条在深色模式下的样式，添加新图标

--- examples/state_tool_tip/demo.py
+++ examples/state_tool_tip/demo.py
@@ -2,13 +2,15 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget
 
-from qfluentwidgets import StateToolTip, PushButton
+from qfluentwidgets import StateToolTip, PushButton, setTheme, Theme
 
 
 class Demo(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
+        # setTheme(Theme.DARK)
+
         self.resize(800, 300)
         self.btn = PushButton('Click Me', parent=self)
         self.btn.move(360, 225)
--- qfluentwidgets/components/widgets/state_tool_tip.py
+++ qfluentwidgets/components/widgets/state_tool_tip.py
@@ -2,10 +2,48 @@
 from PyQt6.QtCore import QEasingCurve, QPropertyAnimation, Qt, QTimer, pyqtSignal, QSize, QPoint, QRectF
 from PyQt6.QtGui import QPainter
 from PyQt6.QtWidgets import QLabel, QWidget, QToolButton, QGraphicsOpacityEffect
-from PyQt6.QtSvgWidgets import QSvgWidget
 
-from ...common import setStyleSheet, drawSvgIcon
-from ...components.widgets.three_state_button import ThreeStateButton, ButtonState
+from ...common import setStyleSheet, drawSvgIcon, isDarkTheme, Theme
+from ...common.icon import FluentIcon as FIF
+
+
+class StateCloseButton(QToolButton):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setFixedSize(12, 12)
+        self.isPressed = False
+        self.isEnter = False
+
+    def enterEvent(self, e):
+        self.isEnter = True
+        self.update()
+
+    def leaveEvent(self, e):
+        self.isEnter = False
+        self.isPressed = False
+        self.update()
+
+    def mousePressEvent(self, e):
+        self.isPressed = True
+        self.update()
+        super().mousePressEvent(e)
+
+    def mouseReleaseEvent(self, e):
+        self.isPressed = False
+        self.update()
+        super().mouseReleaseEvent(e)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.RenderHint.Antialiasing)
+        if self.isPressed:
+            painter.setOpacity(0.6)
+        elif self.isEnter:
+            painter.setOpacity(0.8)
+
+        theme = Theme.DARK if not isDarkTheme() else Theme.LIGHT
+        FIF.CLOSE.render(painter, self.rect(), theme)
 
 
 class StateToolTip(QWidget):
@@ -33,19 +71,10 @@ class StateToolTip(QWidget):
         self.titleLabel = QLabel(self.title, self)
         self.contentLabel = QLabel(self.content, self)
         self.rotateTimer = QTimer(self)
-        self.closeTimer = QTimer(self)
-        self.animation = QPropertyAnimation(self, b"windowOpacity")
-        self.busyIconPath = ":/qfluentwidgets/images/state_tool_tip/running.svg"
-        self.doneIconPath = ":/qfluentwidgets/images/state_tool_tip/completed.svg"
-        self.closeButton = ThreeStateButton(
-            {
-                ButtonState.NORMAL: ':/qfluentwidgets/images/state_tool_tip/close_normal.svg',
-                ButtonState.HOVER: ':/qfluentwidgets/images/state_tool_tip/close_hover.svg',
-                ButtonState.PRESSED: ':/qfluentwidgets/images/state_tool_tip/close_pressed.svg',
-            },
-            parent=self,
-            buttonSize=(12, 12)
-        )
+
+        self.opacityEffect = QGraphicsOpacityEffect(self)
+        self.animation = QPropertyAnimation(self.opacityEffect, b"opacity")
+        self.closeButton = StateCloseButton(self)
 
         self.isDone = False
         self.rotateAngle = 0
@@ -56,14 +85,14 @@ class StateToolTip(QWidget):
     def __initWidget(self):
         """ initialize widgets """
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground)
+        self.setGraphicsEffect(self.opacityEffect)
+        self.opacityEffect.setOpacity(1)
         self.rotateTimer.setInterval(50)
-        self.closeTimer.setInterval(1000)
         self.contentLabel.setMinimumWidth(200)
 
         # connect signal to slot
         self.closeButton.clicked.connect(self.__onCloseButtonClicked)
         self.rotateTimer.timeout.connect(self.__rotateTimerFlowSlot)
-        self.closeTimer.timeout.connect(self.__slowlyClose)
 
         self.__setQss()
         self.__initLayout()
@@ -106,15 +135,15 @@ class StateToolTip(QWidget):
         """ set the state of tooltip """
         self.isDone = isDone
         self.update()
-        if self.isDone:
-            self.closeTimer.start()
+        if isDone:
+            QTimer.singleShot(1000, self.__fadeOut)
 
     def __onCloseButtonClicked(self):
         """ close button clicked slot """
         self.closedSignal.emit()
         self.hide()
 
-    def __slowlyClose(self):
+    def __fadeOut(self):
         """ fade out """
         self.rotateTimer.stop()
         self.animation.setEasingCurve(QEasingCurve.Type.Linear)
@@ -133,9 +162,9 @@ class StateToolTip(QWidget):
         """ get suitable position in main window """
         for i in range(10):
             dy = i*(self.height() + 16)
-            pos = QPoint(self.window().width() - self.width() - 24, 50+dy)
-            widget = self.window().childAt(pos + QPoint(2, 2))
-            if isinstance(widget, (StateToolTip, ToastToolTip)):
+            pos = QPoint(self.parent().width() - self.width() - 24, 50+dy)
+            widget = self.parent().childAt(pos + QPoint(2, 2))
+            if isinstance(widget, StateToolTip):
                 pos += QPoint(0, self.height() + 16)
             else:
                 break
@@ -148,134 +177,12 @@ class StateToolTip(QWidget):
         painter = QPainter(self)
         painter.setRenderHints(QPainter.RenderHint.Antialiasing)
         painter.setPen(Qt.PenStyle.NoPen)
+        theme = Theme.DARK if not isDarkTheme() else Theme.LIGHT
+
         if not self.isDone:
             painter.translate(19, 18)
             painter.rotate(self.rotateAngle)
-            drawSvgIcon(self.busyIconPath, painter, QRectF(-8, -8, 16, 16))
+            FIF.SYNC.render(painter, QRectF(-8, -8, 16, 16), theme)
         else:
-            drawSvgIcon(self.doneIconPath, painter, QRectF(11, 10, 16, 16))
+            FIF.COMPLETED.render(painter, QRectF(11, 10, 16, 16), theme)
 
-
-class ToastToolTip(QWidget):
-    """ Toast tooltip """
-
-    SUCCESS = "completed"
-    WARNING = "info"
-
-    def __init__(self, title, content, icon, parent=None):
-        """
-        Parameters
-        ----------
-        title: str
-            title of tooltip
-
-        content: str
-            content of tooltip
-
-        icon: str
-            icon of toast, can be `completed` or `info`
-
-        parant:
-            parent window
-        """
-        super().__init__(parent)
-        self.title = title
-        self.content = content
-        self.icon = f":/qfluentwidgets/images/state_tool_tip/{icon}.svg"
-
-        self.titleLabel = QLabel(self.title, self)
-        self.contentLabel = QLabel(self.content, self)
-        self.iconLabel = QSvgWidget(self.icon, self)
-        self.closeTimer = QTimer(self)
-        self.opacityEffect = QGraphicsOpacityEffect(self)
-        self.opacityAni = QPropertyAnimation(self.opacityEffect, b"opacity")
-        self.slideAni = QPropertyAnimation(self, b'pos')
-        self.closeButton = ThreeStateButton(
-            {
-                ButtonState.NORMAL: ':/qfluentwidgets/images/state_tool_tip/close_normal.svg',
-                ButtonState.HOVER: ':/qfluentwidgets/images/state_tool_tip/close_hover.svg',
-                ButtonState.PRESSED: ':/qfluentwidgets/images/state_tool_tip/close_pressed.svg',
-            },
-            parent=self,
-            buttonSize=(12, 12)
-        )
-
-        self.__initWidget()
-
-    def __initWidget(self):
-        """ initialize widgets """
-        self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground)
-        self.closeButton.setFixedSize(QSize(11, 11))
-        self.closeButton.setIconSize(QSize(11, 11))
-        self.closeTimer.setInterval(2000)
-        self.contentLabel.setMinimumWidth(180)
-
-        self.iconLabel.resize(16, 16)
-        self.iconLabel.move(12, 10)
-
-        self.setGraphicsEffect(self.opacityEffect)
-        self.opacityEffect.setOpacity(1)
-
-        # connect signal to slot
-        self.closeButton.clicked.connect(self.hide)
-        self.closeTimer.timeout.connect(self.__fadeOut)
-
-        self.__setQss()
-        self.__initLayout()
-
-    def __initLayout(self):
-        """ initialize layout """
-        self.setFixedSize(max(self.titleLabel.width(),
-                          self.contentLabel.width()) + 72, 51)
-        self.titleLabel.move(32, 9)
-        self.contentLabel.move(12, 27)
-        self.closeButton.move(self.width() - 24, 19)
-
-    def __setQss(self):
-        """ set style sheet """
-        self.titleLabel.setObjectName("titleLabel")
-        self.contentLabel.setObjectName("contentLabel")
-        setStyleSheet(self, 'state_tool_tip')
-        self.titleLabel.adjustSize()
-        self.contentLabel.adjustSize()
-
-    def __fadeOut(self):
-        """ fade out """
-        self.opacityAni.setDuration(300)
-        self.opacityAni.setStartValue(1)
-        self.opacityAni.setEndValue(0)
-        self.opacityAni.finished.connect(self.deleteLater)
-        self.opacityAni.start()
-
-    def getSuitablePos(self):
-        """ get suitable position in main window """
-        for i in range(10):
-            dy = i*(self.height() + 16)
-            pos = QPoint(self.window().width() - self.width() - 24, 50+dy)
-            widget = self.window().childAt(pos + QPoint(2, 2))
-            if isinstance(widget, (StateToolTip, ToastToolTip)):
-                pos += QPoint(0, self.height() + 16)
-            else:
-                break
-
-        return pos
-
-    def showEvent(self, e):
-        pos = self.getSuitablePos()
-        self.slideAni.setDuration(200)
-        self.slideAni.setEasingCurve(QEasingCurve.Type.OutQuad)
-        self.slideAni.setStartValue(QPoint(self.window().width(), pos.y()))
-        self.slideAni.setEndValue(pos)
-        self.slideAni.start()
-        super().showEvent(e)
-        self.closeTimer.start()
-
-    @classmethod
-    def success(cls, title: str, content: str, parent=None):
-        """ show a success toast """
-        cls(title, content, cls.SUCCESS, parent).show()
-
-    @classmethod
-    def warn(cls, title: str, content: str, parent=None):
-        """ show a warning toast """
-        cls(title, content, cls.WARNING, parent).show()
