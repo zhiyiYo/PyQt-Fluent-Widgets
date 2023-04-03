a1a57bdb105912e9495b0db8c926ff2d9d1537d5
zhiyiYo <1319158137@qq.com>
2023-04-03 18:41:24 +0800

    更新文档

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,8 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+autodoc_mock_imports = ['PyQt5']
+
 # configuration for 'autoapi.extension'
 autoapi_type = 'python'
 autoapi_dirs = ['../../qfluentwidgets']
