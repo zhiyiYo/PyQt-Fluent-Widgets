23d990cdf77ea83f2c7de3093d9bb3b5252163be
zhiyiYo <1319158137@qq.com>
2023-04-05 17:49:43 +0800

    修复流式布局的问题

--- examples/gallery/app/view/icon_interface.py
+++ examples/gallery/app/view/icon_interface.py
@@ -133,7 +133,7 @@ class IconCardView(QWidget):
 
         self.vBoxLayout = QVBoxLayout(self)
         self.hBoxLayout = QHBoxLayout(self.view)
-        self.flowLayout = FlowLayout(self.scrollWidget)
+        self.flowLayout = FlowLayout(self.scrollWidget, isTight=True)
 
         self.cards = []     # type:List[IconCard]
         self.icons = []
