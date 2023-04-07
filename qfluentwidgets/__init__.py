9feab7a1df53fc817c36144c9352160f765c6ccd
zhiyiYo <1319158137@qq.com>
2023-04-07 20:01:05 +0800

    添加更多下拉框 api

--- qfluentwidgets/__init__.py
+++ qfluentwidgets/__init__.py
@@ -12,7 +12,7 @@ Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/Py
 :license: GPLv3, see LICENSE for more details.
 """
 
-__version__ = "0.6.5"
+__version__ = "0.6.7"
 
 from .components import *
 from .common import *
