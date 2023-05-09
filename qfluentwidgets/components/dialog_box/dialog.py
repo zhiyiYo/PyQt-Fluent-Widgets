3ef148671eacc2fdaf204f633ff0e46508d69663
LittleNyima <littlenyima@163.com>
2023-05-09 11:14:00 +0800

    fix arguments of Ui_MessageBox.__init__ method

--- qfluentwidgets/components/dialog_box/dialog.py
+++ qfluentwidgets/components/dialog_box/dialog.py
@@ -17,6 +17,9 @@ class Ui_MessageBox:
     yesSignal = Signal()
     cancelSignal = Signal()
 
+    def __init__(self, parent):
+        self.parent = parent
+
     def _setUpUi(self, title, content, parent):
         self.content = content
         self.titleLabel = QLabel(title, parent)
