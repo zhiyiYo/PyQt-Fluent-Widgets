15a9d19c8fbd3d1b8be0a540749e2ff15f4b246c
zhiyiYo <1319158137@qq.com>
2023-05-14 21:57:54 +0800

    fixes #200

--- examples/line_edit/demo.py
+++ examples/line_edit/demo.py
@@ -1,26 +1,27 @@
 # coding:utf-8
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
 
-from qfluentwidgets import LineEdit, PushButton, TextEdit
+from qfluentwidgets import LineEdit, PushButton, SearchLineEdit
 
 
 class Demo(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.lineEdit = LineEdit(self)
-        self.button = PushButton('按钮', self)
-        self.textEdit = TextEdit(self)
+        self.hBoxLayout = QHBoxLayout(self)
+        self.lineEdit = SearchLineEdit(self)
+        self.button = PushButton('Search', self)
 
-        self.resize(500, 500)
-        self.textEdit.move(110, 200)
-        self.lineEdit.move(110, 50)
-        self.button.move(320, 52)
+        self.resize(400, 400)
+        self.hBoxLayout.setAlignment(Qt.AlignCenter)
+        self.hBoxLayout.addWidget(self.lineEdit, 0, Qt.AlignCenter)
+        self.hBoxLayout.addWidget(self.button, 0, Qt.AlignCenter)
 
-        self.lineEdit.resize(200, 33)
+        self.lineEdit.setFixedSize(200, 33)
         self.lineEdit.setClearButtonEnabled(True)
+        self.lineEdit.setPlaceholderText('Search icon')
 
 
 if __name__ == '__main__':
