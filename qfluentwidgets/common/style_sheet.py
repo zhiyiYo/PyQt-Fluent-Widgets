395279cb525a39c6108064a68ad0e4f565128ccc
zhiyiYo <1319158137@qq.com>
2023-05-19 14:09:59 +0800

    添加 Pivot

--- qfluentwidgets/common/style_sheet.py
+++ qfluentwidgets/common/style_sheet.py
@@ -87,6 +87,7 @@ class FluentStyleSheet(StyleSheetBase, Enum):
     """ Fluent style sheet """
 
     MENU = "menu"
+    PIVOT = "pivot"
     BUTTON = "button"
     DIALOG = "dialog"
     SLIDER = "slider"
