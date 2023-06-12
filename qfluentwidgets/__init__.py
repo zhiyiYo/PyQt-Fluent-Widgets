f9e9654dbaf32709884e9b572528c9fb9dae0036
zhiyiYo <1319158137@qq.com>
2023-06-12 14:25:09 +0800

    修复 Mac 系统下菜单颜色和应用主题不匹配的问题 (#274)

--- qfluentwidgets/__init__.py
+++ qfluentwidgets/__init__.py
@@ -12,7 +12,7 @@ Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/ma
 :license: GPLv3, see LICENSE for more details.
 """
 
-__version__ = "0.9.3"
+__version__ = "0.9.4"
 
 from .components import *
 from .common import *
