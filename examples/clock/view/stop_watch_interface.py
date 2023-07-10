63a098ef08114769d0e25a1a8ddaf82ec7a7fa6a
zhiyiYo <1319158137@qq.com>
2023-07-10 20:30:57 +0800

    添加搭配 QtDesigner 的多子界面示例

---
+++ examples/clock/view/stop_watch_interface.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap, QPainter, QColor
+from PyQt6.QtWidgets import QWidget
+
+from qfluentwidgets import FluentIcon, setFont
+from view.Ui_StopWatchInterface import Ui_StopWatchInterface
+
+
+class StopWatchInterface(Ui_StopWatchInterface, QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.setupUi(self)
+
+        self.startButton.setIcon(FluentIcon.POWER_BUTTON)
+        self.flagButton.setIcon(FluentIcon.FLAG)
+        self.restartButton.setIcon(FluentIcon.CANCEL)
+
+        setFont(self.timeLabel, 100)
+        self.timeLabel.setTextColor(QColor(95, 95, 95), QColor(206, 206, 206))
+        self.hourLabel.setTextColor(QColor(95, 95, 95), QColor(206, 206, 206))
+        self.minuteLabel.setTextColor(QColor(95, 95, 95), QColor(206, 206, 206))
+        self.secondLabel.setTextColor(QColor(95, 95, 95), QColor(206, 206, 206))
+
