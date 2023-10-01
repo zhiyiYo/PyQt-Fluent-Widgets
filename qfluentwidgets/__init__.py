4f1da1617b885ef48cc0bc916ef2769ac5a33948
zhiyiYo <1319158137@qq.com>
2023-10-01 17:58:31 +0800

    修复 `ElevatedCardWidget` 滚动卡顿问题 (#526)

--- qfluentwidgets/__init__.py
+++ qfluentwidgets/__init__.py
@@ -4,7 +4,7 @@ PySide6-Fluent-Widgets
 A fluent design widgets library based on PySide6.
 
 Documentation is available in the docstrings and
-online at https://pyqt-fluent-widgets.readthedocs.io.
+online at https://https://qfluentwidgets.com.
 
 Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/PySide6/examples.
 
@@ -12,7 +12,7 @@ Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/Py
 :license: GPLv3, see LICENSE for more details.
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 from .components import *
 from .common import *
