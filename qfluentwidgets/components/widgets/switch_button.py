8a7ad2bedab0508952c410269826d2b6a0944cb1
zhiyiYo <1319158137@qq.com>
2023-10-14 10:29:50 +0800

    修复 ExpandSettingCard 控件出现错位的问题 (#561)

--- qfluentwidgets/components/widgets/switch_button.py
+++ qfluentwidgets/components/widgets/switch_button.py
@@ -111,14 +111,15 @@ class Indicator(ToolButton):
 
             return QColor(255, 255, 255, 96) if isDark else QColor(0, 0, 0, 91)
 
-    @Property(float)
-    def sliderX(self):
+    def getSliderX(self):
         return self._sliderX
 
-    @sliderX.setter
-    def sliderX(self, x):
-        self._sliderX = x
+    def setSliderX(self, x):
+        self._sliderX = max(x, 5)
         self.update()
+
+    sliderX = Property(float, getSliderX, setSliderX)
+
 
 class IndicatorPosition(Enum):
     """ Indicator position """
