2824401f466b22d9e01e63710e28782e2d78ecb9
zhiyiYo <1319158137@qq.com>
2023-11-09 12:38:03 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name="PyQt6-Fluent-Widgets",
-    version="1.3.6",
+    version="1.3.7",
     keywords="pyqt6 fluent widgets",
     author="zhiyiYo",
     author_email="shokokawaii@outlook.com",
