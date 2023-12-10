741c71de43b53f9744a30a2f347c3be1966c6a4c
zhiyiYo <1319158137@qq.com>
2023-12-10 22:12:45 +0800

    修复复选框图标模糊问题 (#661)

--- qfluentwidgets/components/widgets/tree_view.py
+++ qfluentwidgets/components/widgets/tree_view.py
@@ -1,10 +1,11 @@
 # coding:utf-8
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtCore import Qt, QSize, QRectF, QModelIndex
 from PyQt5.QtGui import QPainter, QColor, QPalette
-from PyQt5.QtWidgets import QTreeWidget, QStyledItemDelegate, QStyle, QTreeView, QApplication
+from PyQt5.QtWidgets import QTreeWidget, QStyledItemDelegate, QStyle, QTreeView, QApplication, QStyleOptionViewItem
 
 from ...common.style_sheet import FluentStyleSheet, themeColor, isDarkTheme, setCustomStyleSheet
 from ...common.font import getFont
+from .check_box import CheckBoxIcon
 from .scroll_area import SmoothScrollDelegate
 
 
@@ -18,6 +19,9 @@ class TreeItemDelegate(QStyledItemDelegate):
         painter.setRenderHints(
             QPainter.Antialiasing | QPainter.TextAntialiasing)
         super().paint(painter, option, index)
+
+        if index.data(Qt.CheckStateRole) is not None:
+            self._drawCheckBox(painter, option, index)
 
         if not (option.state & (QStyle.State_Selected | QStyle.State_MouseOver)):
             return
@@ -38,6 +42,36 @@ class TreeItemDelegate(QStyledItemDelegate):
             painter.drawRoundedRect(4, 9+option.rect.y(), 3, h - 13, 1.5, 1.5)
 
         painter.restore()
+
+    def _drawCheckBox(self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex):
+        painter.save()
+        checkState = index.data(Qt.CheckStateRole)
+
+        isDark = isDarkTheme()
+
+        r = 4
+        x = option.rect.x() + 23
+        y = option.rect.center().y() - 9
+        rect = QRectF(x, y, 19, 19)
+
+        if checkState == Qt.CheckState.Unchecked:
+            painter.setBrush(QColor(0, 0, 0, 26)
+                             if isDark else QColor(0, 0, 0, 6))
+            painter.setPen(QColor(255, 255, 255, 142)
+                           if isDark else QColor(0, 0, 0, 122))
+            painter.drawRoundedRect(rect, r, r)
+        else:
+            painter.setPen(themeColor())
+            painter.setBrush(themeColor())
+            painter.drawRoundedRect(rect, r, r)
+
+            if checkState == Qt.CheckState.Checked:
+                CheckBoxIcon.ACCEPT.render(painter, rect)
+            else:
+                CheckBoxIcon.PARTIAL_ACCEPT.render(painter, rect)
+
+        painter.restore()
+
 
     def initStyleOption(self, option, index):
         super().initStyleOption(option, index)
