e984042b44bc1f45107ddb7a1fae629ecc5a0000
zhiyiYo <1319158137@qq.com>
2024-03-13 00:03:06 +0800

    更新版本号

--- qfluentwidgets/__init__.py
+++ qfluentwidgets/__init__.py
@@ -12,7 +12,7 @@ Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/ma
 :license: GPLv3 for non-commercial project, see README for more details.
 """
 
-__version__ = "1.5.1"
+__version__ = "1.5.2"
 __author__ = "zhiyiYo"
 
 from .components import *
