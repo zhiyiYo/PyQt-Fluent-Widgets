e21a8b3b73f492203695d165b94a18f3fcc0519f
rainzee wang <rainzee.w@gmail.com>
2024-03-29 22:52:50 +0800

    fix: import List for type hints

--- qfluentwidgets/components/navigation/navigation_widget.py
+++ qfluentwidgets/components/navigation/navigation_widget.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-from typing import Union
+from typing import Union, List
 
 from PyQt5.QtCore import (Qt, pyqtSignal, QRect, QRectF, QPropertyAnimation, pyqtProperty, QMargins,
                           QEasingCurve, QPoint, QEvent)
