65e09433a4ca08ac6a7ab01a48f6839814226ef9
之一 <1319158137@qq.com>
2025-05-17 13:58:22 +0800

    更新版本号为 v1.8.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name="PyQt-Fluent-Widgets",
-    version="1.8.0",
+    version="1.8.1",
     keywords="pyqt fluent widgets",
     author="zhiyiYo",
     author_email="shokokawaii@outlook.com",
