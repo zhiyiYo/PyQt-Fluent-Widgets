60aa40aaaa2c51a6f1e43d5e247d692fd8bbcee7
zhiyiYo <1319158137@qq.com>
2025-09-27 15:54:55 +0800

    添加自定义组件库字体的功能

--- qfluentwidgets/common/font.py
+++ qfluentwidgets/common/font.py
@@ -1,6 +1,28 @@
 # coding: utf-8
+from typing import List
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QWidget
+
+from .config import qconfig
+
+
+def setFontFamilies(families: List[str], save=False):
+    """ set the font families used by all widgets
+
+    Parameters
+    ----------
+    families: List[str]
+        font family names, the default value is `['Segoe UI', 'Microsoft YaHei', 'PingFang SC']`
+
+    save: bool
+        whether to save the change to config file
+    """
+    qconfig.set(qconfig.fontFamilies, families, save)
+
+
+def fontFamilies() -> List[str]:
+    """ Returns the font families used by all widgets """
+    return qconfig.get(qconfig.fontFamilies)
 
 
 def setFont(widget: QWidget, fontSize=14, weight=QFont.Normal):
@@ -32,7 +54,17 @@ def getFont(fontSize=14, weight=QFont.Normal):
         font weight
     """
     font = QFont()
-    font.setFamilies(['Segoe UI', 'Microsoft YaHei', 'PingFang SC'])
+    font.setFamilies(qconfig.get(qconfig.fontFamilies))
     font.setPixelSize(fontSize)
     font.setWeight(weight)
     return font
+
+
+def fontStyleSheet(font: QFont):
+    """ Returns the style sheet of font """
+    families = []
+    for family in font.families():
+        families.append(f"'{family}'")
+
+    qss = f"font: {font.pixelSize()}px {','.join(families)}"
+    return qss
