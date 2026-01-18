f6c5cad5cf5ab97ff0345aa3a49ebdfd8f031e11
之一 <1319158137@qq.com>
2026-01-18 17:12:55 +0800

    chore: 更新到 v1.11.0

---
+++ qfluentwidgets/_rc/i18n/qfluentwidgets.pl_PL.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>ColorDialog</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <source>Edit Color</source>
+        <translation>Edytuj kolor</translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation>Czerwony</translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation>Niebieski</translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation>Zielony</translation>
+    </message>
+    <message>
+        <source>Opacity</source>
+        <translation>Przezroczystość</translation>
+    </message>
+</context>
+<context>
+    <name>MessageDialog</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+</context>
+<context>
+    <name>MessageBox</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+</context>
+<context>
+    <name>FolderListDialog</name>
+    <message>
+        <source>Done</source>
+        <translation>Gotowe</translation>
+    </message>
+    <message>
+        <source>Choose folder</source>
+        <translation>Wybierz folder</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the folder?</source>
+        <translation>Czy na pewno chcesz usunąć ten folder?</translation>
+    </message>
+    <message>
+        <source>If you delete the </source>
+        <translation>Jeśli usuniesz </translation>
+    </message>
+    <message>
+        <source> folder and remove it from the list, the folder will no longer appear in the list, but will not be deleted.</source>
+        <translation> folder i usuniesz go z listy, nie będzie on już na niej widoczny, ale nie zostanie trwale usunięty z dysku.</translation>
+    </message>
+</context>
+<context>
+    <name>SwitchSettingCard</name>
+    <message>
+        <source>Off</source>
+        <translation>Wył.</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>Wł.</translation>
+    </message>
+</context>
+<context>
+    <name>CustomColorSettingCard</name>
+    <message>
+        <source>Custom color</source>
+        <translation>Kolor niestandardowy</translation>
+    </message>
+    <message>
+        <source>Default color</source>
+        <translation>Kolor domyślny</translation>
+    </message>
+    <message>
+        <source>Choose color</source>
+        <translation>Wybierz kolor</translation>
+    </message>
+</context>
+<context>
+    <name>ColorPickerButton</name>
+    <message>
+        <source>Choose </source>
+        <translation>Wybierz </translation>
+    </message>
+</context>
+<context>
+    <name>FolderListSettingCard</name>
+    <message>
+        <source>Add folder</source>
+        <translation>Dodaj folder</translation>
+    </message>
+    <message>
+        <source>Choose folder</source>
+        <translation>Wybierz folder</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the folder?</source>
+        <translation>Czy na pewno chcesz usunąć ten folder?</translation>
+    </message>
+    <message>
+        <source>If you delete the </source>
+        <translation>Jeśli usuniesz </translation>
+    </message>
+    <message>
+        <source> folder and remove it from the list, the folder will no longer appear in the list, but will not be deleted.</source>
+        <translation> folder i usuniesz go z listy, nie będzie on już na niej widoczny, ale nie zostanie trwale usunięty z dysku.</translation>
+    </message>
+</context>
+<context>
+    <name>LineEditMenu</name>
+    <message>
+        <source>Cut</source>
+        <translation>Wytnij</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopiuj</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Wklej</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <source>Select all</source>
+        <translation>Zaznacz wszystko</translation>
+    </message>
+</context>
+<context>
+    <name>TextEditMenu</name>
+    <message>
+        <source>Cut</source>
+        <translation>Wytnij</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopiuj</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Wklej</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <source>Select all</source>
+        <translation>Zaznacz wszystko</translation>
+    </message>
+</context>
+<context>
+    <name>TimePicker</name>
+    <message>
+        <source>hour</source>
+        <translation>godzina</translation>
+    </message>
+    <message>
+        <source>minute</source>
+        <translation>minuta</translation>
+    </message>
+    <message>
+        <source>second</source>
+        <translation>sekunda</translation>
+    </message>
+</context>
+<context>
+    <name>AMPMFormatter</name>
+    <message>
+        <source>AM</source>
+        <translation>AM</translation>
+    </message>
+    <message>
+        <source>PM</source>
+        <translation>PM</translation>
+    </message>
+</context>
+<context>
+    <name>AMTimePicker</name>
+    <message>
+        <source>hour</source>
+        <translation>godzina</translation>
+    </message>
+    <message>
+        <source>minute</source>
+        <translation>minuta</translation>
+    </message>
+    <message>
+        <source>second</source>
+        <translation>sekunda</translation>
+    </message>
+    <message>
+        <source>AM</source>
+        <translation>AM</translation>
+    </message>
+    <message>
+        <source>PM</source>
+        <translation>PM</translation>
+    </message>
+</context>
+</TS>
