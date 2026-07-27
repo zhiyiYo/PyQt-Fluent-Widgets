9b0cfa340b4f47191f50b37ae0d9b3d358879c9f
bzorn <v.klimenko.2137@gmail.com>
2026-07-27 15:14:07 +0300

    style: format imports

--- qfluentwidgets/common/__init__.py
+++ qfluentwidgets/common/__init__.py
@@ -1,10 +1,34 @@
 from .config import *
 from .font import setFont, getFont, setFontFamilies, fontFamilies, fontStyleSheet
 from .auto_wrap import TextWrap
-from .icon import Action, Icon, getIconColor, drawSvgIcon, FluentIcon, drawIcon, FluentIconBase, writeSvg, FluentFontIconBase
-from .style_sheet import (setStyleSheet, getStyleSheet, setTheme, ThemeColor, themeColor,
-                          setThemeColor, applyThemeColor, FluentStyleSheet, StyleSheetBase,
-                          StyleSheetFile, StyleSheetCompose, CustomStyleSheet, toggleTheme, setCustomStyleSheet, renderQss)
+from .icon import (
+    Action,
+    Icon,
+    getIconColor,
+    drawSvgIcon,
+    FluentIcon,
+    drawIcon,
+    FluentIconBase,
+    writeSvg,
+    FluentFontIconBase,
+)
+from .style_sheet import (
+    setStyleSheet,
+    getStyleSheet,
+    setTheme,
+    ThemeColor,
+    themeColor,
+    setThemeColor,
+    applyThemeColor,
+    FluentStyleSheet,
+    StyleSheetBase,
+    StyleSheetFile,
+    StyleSheetCompose,
+    CustomStyleSheet,
+    toggleTheme,
+    setCustomStyleSheet,
+    renderQss,
+)
 from .smooth_scroll import SmoothScroll, SmoothMode
 from .translator import FluentTranslator
 from .router import qrouter, Router
